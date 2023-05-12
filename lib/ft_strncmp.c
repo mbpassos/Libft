8747654d13f87df1c1804a1879f3feb4a169633a
mbpassos <mbpassos@HP>
2023-05-12 15:49:22 +0100

    add lib final

--- lib/ft_strncmp.c
+++ lib/ft_strncmp.c
@@ -15,7 +15,7 @@
 // The function performs a simple binary comparison of the strings based 
 // on the numerical values of their characters.It returns an integer less than, 
 // equal to, or greater than zero if s1 is  found, respectively, to be less 
-// than, to match, or be greater than s2. The strncmp() function compares 
+// than, to match, or be greater than s2. The strncmp() function compares :q
 // only the first (at most) n bytes of s1 and s2.
 #include "lift.h"
 
@@ -36,6 +36,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 // {
 //     char str1[] = "Hello";
 //     char str2[] = "World!";
-//     printf("st2 > str1 so negative:%d", ft_strncmp(str1, str2, 10));
+
+//     printf("st2 > str1 should be negative:%d", ft_strncmp(str1, str2, 10));
 //     return (0);
 // }
